503bcf6cd2b7c362ffe72c69958cd7d1b6cfacd8
Nargiz Ganiyeva <tu7lyw21z@aztu.local>
2022-03-28 13:37:06 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    class Product
+    {
+        public int Id { get; }
+        private static int _id;
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int StockCount { get; set; }
+        public int Count { get; set; }
+
+        public Product()
+        {
+            _id++;
+            Id = _id;
+            this.Name = Name;
+            this.Price = Price;
+            this.StockCount = StockCount;
+            this.Count = Count;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name:{Name}\nPrice:{Price}\nStockCount:{StockCount}\nCount{Count}");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1.Models.Exceptions;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Custom newCustom = null;
+
+            try
+            {
+                newCustom = new Custom();
+                newCustom.CapacityLimitException = 123;
+
+                ValidateCustom(newCustom);
+            }
+            catch (CustomExceptions ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        private static void ValidateCustom(object newStudent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    }
+
